fa584097e793b848264eccdc5725de0655a3a78c
y-takagiiii <takagiyuri0712@gmail.com>
2025-11-13 16:02:32 +0900

    加算と条件式の型チェックを追加

--- 02/arith.ts
+++ 02/arith.ts
@@ -1,6 +1,8 @@
 
 // trueリテラル、falseリテラル、条件演算子、数値リテラル、足し算など単純な処理のみ扱える言語仕様
 
+import { parseArith } from "tiny-ts-parser";
+
 // 言語仕様を構文木で表現
 // true、false、数値は葉ノード
 // 足し算は左右に子ノードを持つaddノード
@@ -30,5 +32,26 @@ function typecheck(t: Term): Type {
       return { tag: "Boolean" }
     case "number":
       return { tag: "Number" }
+    case "add": {
+      const leftType = typecheck(t.left);
+      if (leftType.tag !== "Number") throw "number expected";
+      const rightType = typecheck(t.right);
+      if (rightType.tag !== "Number") throw "number expected";
+      return { tag: "Number" };
+    }
+    case "if": {
+      // 条件式がbooleanかチェック
+      const condType = typecheck(t.cond);
+      if (condType.tag !== "Boolean") throw "boolean expected"
+      // 条件演算子の返す型が一致するかチェック
+      const thnType = typecheck(t.thn);
+      const elsType = typecheck(t.els);
+      if (thnType.tag !== elsType.tag) {
+        throw "then and else have different types"
+      }
+      return thnType;
+    }
   }
 }
+
+console.log(typecheck(parseArith("true ? 2 : 3")));
