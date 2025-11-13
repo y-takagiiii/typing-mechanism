497610c6a9abecbe7cd7089da8fdc3d68a44b366
y-takagiiii <takagiyuri0712@gmail.com>
2025-11-13 19:05:30 +0900

    型の等価性をチェックするtypeEq関数を作成

--- 03/basic.ts
+++ 03/basic.ts
@@ -47,4 +47,24 @@ function typecheck(t: Term): Type {
   }
 }
 
+// 型の等価判定をする関数
+function typeEq(ty1: Type, ty2: Type): boolean {
+  switch (ty2.tag) {
+    case "Boolean":
+      return ty1.tag === "Boolean";
+    case "Number":
+      return ty1.tag === "Number";
+    // 関数の等価判定は1.仮引数の個数が同じ、2.仮引数の型が同じ、3.返り値の方が同じ
+    case "Func": {
+      if (ty1.tag !== "Func") return false;
+      if (ty1.params.length !== ty2.params.length) return false;
+      for (let i = 0; i < ty1.params.length; i++) {
+        if (!typeEq(ty1.params[i].type, ty2.params[i].type)) return false;
+      }
+      if (!typeEq(ty1.retType, ty2.retType)) return false;
+      return true;
+    }
+  }
+}
+
 console.log(typecheck(parseArith("1 ? true : false"))); // => { tag: "Boolean" }
