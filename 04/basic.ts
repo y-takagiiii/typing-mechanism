b3b4b751525b62bf7726ff6e227f037848ca1405
y-takagiiii <takagiyuri0712@gmail.com>
2025-11-15 16:16:04 +0900

    逐次実行の型チェックを追加

---
+++ 04/basic.ts
@@ -0,0 +1,100 @@
+import { parseArith, parseBasic } from "tiny-ts-parser";
+
+type Term =
+  | { tag: "true" }
+  | { tag: "false" }
+  | { tag: "number"; n: number }
+  | { tag: "if"; cond: Term; thn: Term; els: Term }
+  | { tag: "add"; left: Term; right: Term }
+  | { tag: "var"; name: string }
+  | { tag: "func"; params: Param[]; body: Term }
+  | { tag: "call"; func: Term; args: Term[] }
+  | { tag: "seq"; body: Term; rest: Term }
+  | { tag: "const"; name: string; init: Term; rest: Term };
+
+type Type =
+  | { tag: "Boolean" }
+  | { tag: "Number" }
+  | { tag: "Func"; params: Param[]; retType: Type };
+
+type Param = { name: string; type: Type };
+
+type TypeEnv = Record<string, Type>;
+
+function typecheck(t: Term, tyEnv: TypeEnv): Type {
+  switch (t.tag) {
+    case "true":
+      return { tag: "Boolean" }
+    case "false":
+      return { tag: "Boolean" }
+    case "number":
+      return { tag: "Number" }
+    case "add": {
+      const leftType = typecheck(t.left, tyEnv);
+      if (leftType.tag !== "Number") throw "number expected";
+      const rightType = typecheck(t.right, tyEnv);
+      if (rightType.tag !== "Number") throw "number expected";
+      return { tag: "Number" };
+    }
+    case "if": {
+      const condType = typecheck(t.cond, tyEnv);
+      if (condType.tag !== "Boolean") throw "boolean expected"
+      const thnType = typecheck(t.thn, tyEnv);
+      const elsType = typecheck(t.els, tyEnv);
+      if (thnType.tag !== elsType.tag) {
+        throw "then and else have different types"
+      }
+      return thnType;
+    }
+    case "var":
+      if (tyEnv[t.name] === undefined)
+        throw new Error(`unknown variable: ${t.name}`);
+      return tyEnv[t.name];
+    case "func":
+      const newTyEnv = { ...tyEnv };
+      for (const { name, type } of t.params) {
+        newTyEnv[name] = type;
+      }
+      const retType = typecheck(t.body, newTyEnv);
+      return { tag: "Func", params: t.params, retType };
+    case "call": {
+      const funcTy = typecheck(t.func, tyEnv);
+      if (funcTy.tag !== "Func") throw new Error("function type expected");
+      if (funcTy.params.length !== t.args.length) {
+        throw new Error("wrong number of arguments");
+      }
+      for (let i = 0; i < t.args.length; i++) {
+        const argTy = typecheck(t.args[i], tyEnv);
+        if (!typeEq(argTy, funcTy.params[i].type)) {
+          throw new Error("parameter type mismatch");
+        }
+      }
+      return funcTy.retType;
+    }
+    case "seq":
+      typecheck(t.body, tyEnv);
+      return typecheck(t.rest, tyEnv);
+    default:
+      throw new Error("not implemented yet")
+  }
+}
+
+function typeEq(ty1: Type, ty2: Type): boolean {
+  switch (ty2.tag) {
+    case "Boolean":
+      return ty1.tag === "Boolean";
+    case "Number":
+      return ty1.tag === "Number";
+    case "Func": {
+      if (ty1.tag !== "Func") return false;
+      if (ty1.params.length !== ty2.params.length) return false;
+      for (let i = 0; i < ty1.params.length; i++) {
+        if (!typeEq(ty1.params[i].type, ty2.params[i].type)) return false;
+      }
+      if (!typeEq(ty1.retType, ty2.retType)) return false;
+      return true;
+    }
+  }
+}
+
+
